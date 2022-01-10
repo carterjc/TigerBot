43e3a1eb6683770a082344f83fdf74c35f693aa4
Carter Costic <cartercostic@gmail.com>
2022-01-10 13:12:58 -0500

    fixed duplication bug

--- bot/deploy-commands.js
+++ bot/deploy-commands.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+// instead of importing client which has some weird side effects, just import the logger class directory
+const { logger } = require('./utils/logger');
 require('dotenv').config();
 
 const commands = [];
@@ -13,27 +15,29 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 
-// global commands take an hour to cache, guild commands are updated instantly
-// applicationCommands(CLIENT_ID) v. applicationGuildCommands(CLIENT_ID, GUILD_ID)
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
+if (process.env.ENV === 'prod') {
+	// global commands take an hour to cache, guild commands are updated instantly
+	// applicationCommands(CLIENT_ID) v. applicationGuildCommands(CLIENT_ID, GUILD_ID)
+	(async () => {
+		try {
+			logger.log('Started refreshing application (/) commands.', 'log');
 
-		await rest.put(
-			Routes.applicationCommands(process.env.CLIENT_ID),
-			{ body: commands },
-		);
+			await rest.put(
+				Routes.applicationCommands(process.env.CLIENT_ID),
+				{ body: commands },
+			);
 
-		console.log('Successfully reloaded application (/) commands.');
-	}
-	catch (error) {
-		console.error(error);
-	}
-})();
-
-if (process.env.ENV === 'dev') {
+			logger.log('Successfully reloaded application (/) commands.', 'log');
+		}
+		catch (error) {
+			logger.log(error, 'error');
+		}
+	})();
+}
+// for dev
+else {
 	// only makes commands available in one guild (good for development) - updated instantly
 	rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
-		.then(() => console.log('Successfully registered application commands.'))
+		.then(() => logger.log('Successfully registered application commands.', 'log'))
 		.catch(console.error);
 }
