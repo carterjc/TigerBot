8c5a92a45ceba67d16fa6f08c7109cd56ced7a54
Carter Costic <cartercostic@gmail.com>
2022-01-14 23:39:53 -0500

    haha julie L

--- bot/commands/ping.js
+++ bot/commands/ping.js
@@ -5,6 +5,7 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
+		if (interaction.user.id === '493180729358942226') return interaction.reply('do this again and gort gets it');
 		return interaction.reply('Pong!');
 	},
 };
