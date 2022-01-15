105a3c52b73c1c09f42dad943489eea430ff6b7a
Carter Costic <cartercostic@gmail.com>
2022-01-15 00:10:17 -0500

    the julie commit ft. gort

--- bot/commands/ping.js
+++ bot/commands/ping.js
@@ -1,11 +1,17 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		if (interaction.user.id === '493180729358942226') return interaction.reply('do this again and gort gets it');
-		return interaction.reply('Pong!');
+		// the julie special
+		const exampleEmbed = new MessageEmbed()
+			.setTitle('gort ded')
+			.setImage('attachment://gort.jpg');
+		if (interaction.user.id === '493180729358942226') return interaction.reply({ embeds: [exampleEmbed], files: ['./data/gort.jpg'] });
+		// regular command
+		return interaction.reply({ embeds: [exampleEmbed], files: ['./data/gort.jpg'] });
 	},
 };
