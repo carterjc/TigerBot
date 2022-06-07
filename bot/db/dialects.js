46e3dac79806aacddba5cb2595cf438711ad34d4
Carter Costic <cartercostic@gmail.com>
2022-06-07 00:05:01 -0400

    added package dialects back and installed pg packages

--- bot/db/dialects.js
+++ bot/db/dialects.js
@@ -2,16 +2,16 @@ module.exports = {
 	postgres: {
 		dialect: 'postgres',
 		name: 'PostgreSQL',
-		// packages: ['pg', 'pg-hstore'],
+		packages: ['pg', 'pg-hstore'],
 	},
 	postgresql: {
 		dialect: 'postgres',
 		name: 'PostgreSQL',
-		// packages: ['pg', 'pg-hstore'],
+		packages: ['pg', 'pg-hstore'],
 	},
 	sqlite: {
 		dialect: 'sqlite',
 		name: 'SQLite',
-		// packages: ['sqlite3'],
+		packages: ['sqlite3'],
 	},
 };
